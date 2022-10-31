227ebe9b7f9cee21c1c1ef397c6c60cdcd1d78d8
Marçal Nebot <mnebot@gmail.com>
2022-10-31 12:03:58 +0100

    atlantis test

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "eu-west-3"
 }
 
 # website::tag::1:: Deploy an EC2 Instance.
@@ -14,7 +14,7 @@ resource "aws_instance" "example" {
   # website::tag::2:: Run an Ubuntu 18.04 AMI on the EC2 instance.
   ami                    = "ami-0d5d9d301c853a04a"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+
 
   # website::tag::3:: When the instance boots, start a web server on port 8080 that responds with "Hello, World!".
   user_data = <<EOF
@@ -24,15 +24,7 @@ nohup busybox httpd -f -p 8080 &
 EOF
 }
 
-# website::tag::4:: Allow the instance to receive requests on port 8080.
-#resource "aws_security_group" "instance" {
-ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+
 
 # website::tag::5:: Output the instance's public IP address.
 output "public_ip" {
